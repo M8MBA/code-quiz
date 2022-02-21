73c3b6a00cf73ffd2d0db55564c5a54a969083dc
M8MBA <sauce.ix@gmail.com>
2022-02-21 16:19:29 -0700

    functionality

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+  {
+    title: "Which one of the following is known as the Equality operator, which is used to check whether the two values are equal or not?",
+    choices: ["=", "===", "==", "&&"],
+    answer: "=="
+  },
+    {
+    title: "Which one of the following symbol is used for creating comments in the javascript?",
+    choices: ["()", "//", "<>", "comment:"],
+    answer: "//"
+  },
+    {
+    title: " A collection of elements of the same data type which may either in order or not, is called _____.",
+    choices: ["string", "array", "object", "rope"],
+    answer: "array"
+  },
+    {
+    title: "The condition in an if / else statement is enclosed with ______.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "parenthesis"
+  },
+];
